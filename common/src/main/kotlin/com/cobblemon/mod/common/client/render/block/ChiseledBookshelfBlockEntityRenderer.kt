1e578e94e5a7ea199305f34fb074b49fb1ccfcec
plastered_crab <jeffw773@gmail.com>
2025-04-22 08:32:35 -0700

    Shelf renders without redish color

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/block/ChiseledBookshelfBlockEntityRenderer.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/block/ChiseledBookshelfBlockEntityRenderer.kt
@@ -23,7 +23,6 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
     private val texture = cobblemonResource("textures/block/chiseled_bookshelf/technical_machine_case.png")
     private val vanillaFaceTexture = ResourceLocation.parse("minecraft:textures/block/chiseled_bookshelf_empty.png")
 
-    // Layout parameters in pixels (relative to 16x16 block face)
     private val slotWidthPixels = 2
     private val slotHeightPixels = 6
     private val textureSize = 16f
@@ -31,7 +30,7 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
     private val leftMargin = 1
     private val topMargin = 1
     private val interRowSpacing = 2
-    private val slotSpacing = 0  // Horizontal spacing between slots if needed
+    private val slotSpacing = 0  // Horizontal spacing between slots if we need to adjust them
 
 
     override fun render(
@@ -42,7 +41,6 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
         light: Int,
         overlay: Int
     ) {
-
         val facing = entity.blockState.getValue(HorizontalDirectionalBlock.FACING)
         val rotation = when (facing) {
             Direction.NORTH -> 0f
@@ -57,7 +55,6 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
         poseStack.mulPose(Axis.YP.rotationDegrees(rotation))
         poseStack.translate(-0.5, -0.5, -0.5)
 
-        // we render the dumb missing face
         renderFrontFaceQuad(poseStack, buffer, vanillaFaceTexture, light)
 
         val slotWidth = slotWidthPixels / textureSize
@@ -97,7 +94,7 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
         width: Float,
         height: Float
     ) {
-        val consumer: VertexConsumer = buffer.getBuffer(RenderType.entitySolid(texture))
+        val consumer: VertexConsumer = buffer.getBuffer(RenderType.text(texture))
         val matrix: Matrix4f = poseStack.last().pose()
 
         val r = FastColor.ARGB32.red(color) / 255f
@@ -105,33 +102,14 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
         val b = FastColor.ARGB32.blue(color) / 255f
         val a = 1.0f
 
-        consumer.addVertex(matrix, 0f, 0f, 0f)
-            .setColor(r, g, b, a)
-            .setUv(0f, 0f)
-            .setUv1(0, 0)
-            .setUv2(light and 0xFFFF, light shr 16)
-            .setNormal(0f, 0f, -1f)
-
-        consumer.addVertex(matrix, 0f, height, 0f)
-            .setColor(r, g, b, a)
-            .setUv(0f, 1f)
-            .setUv1(0, 0)
-            .setUv2(light and 0xFFFF, light shr 16)
-            .setNormal(0f, 0f, -1f)
-
-        consumer.addVertex(matrix, width, height, 0f)
-            .setColor(r, g, b, a)
-            .setUv(1f, 1f)
-            .setUv1(0, 0)
-            .setUv2(light and 0xFFFF, light shr 16)
-            .setNormal(0f, 0f, -1f)
-
-        consumer.addVertex(matrix, width, 0f, 0f)
-            .setColor(r, g, b, a)
-            .setUv(1f, 0f)
-            .setUv1(0, 0)
-            .setUv2(light and 0xFFFF, light shr 16)
-            .setNormal(0f, 0f, -1f)
+        consumer.addVertex(matrix, 0f, 0f, 0f).setColor(r, g, b, a).setUv(0f, 0f)
+            .setUv1(0, 0).setUv2(light and 0xFFFF, light shr 16).setNormal(0f, 0f, -1f)
+        consumer.addVertex(matrix, 0f, height, 0f).setColor(r, g, b, a).setUv(0f, 1f)
+            .setUv1(0, 0).setUv2(light and 0xFFFF, light shr 16).setNormal(0f, 0f, -1f)
+        consumer.addVertex(matrix, width, height, 0f).setColor(r, g, b, a).setUv(1f, 1f)
+            .setUv1(0, 0).setUv2(light and 0xFFFF, light shr 16).setNormal(0f, 0f, -1f)
+        consumer.addVertex(matrix, width, 0f, 0f).setColor(r, g, b, a).setUv(1f, 0f)
+            .setUv1(0, 0).setUv2(light and 0xFFFF, light shr 16).setNormal(0f, 0f, -1f)
     }
 
     private fun renderFrontFaceQuad(
@@ -140,46 +118,21 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
         texture: ResourceLocation,
         light: Int
     ) {
-        val consumer = buffer.getBuffer(RenderType.entitySolid(texture))
+        val consumer = buffer.getBuffer(RenderType.text(texture))
         val matrix = poseStack.last().pose()
 
-        val u0 = 0f
-        val v0 = 0f
-        val u1 = 1f
-        val v1 = 1f
+        val r = 1f
+        val g = 1f
+        val b = 1f
+        val a = 1f
 
-        val r = 1.0f
-        val g = 1.0f
-        val b = 1.0f
-        val a = 1.0f
-
-        // Front face is on Z = 0
-        consumer.addVertex(matrix, 0f, 0f, 0f)
-            .setColor(r, g, b, a)
-            .setUv(u0, v0)
-            .setUv1(0, 0)
-            .setUv2(light and 0xFFFF, light shr 16)
-            .setNormal(0f, 0f, -1f)
-
-        consumer.addVertex(matrix, 0f, 1f, 0f)
-            .setColor(r, g, b, a)
-            .setUv(u0, v1)
-            .setUv1(0, 0)
-            .setUv2(light and 0xFFFF, light shr 16)
-            .setNormal(0f, 0f, -1f)
-
-        consumer.addVertex(matrix, 1f, 1f, 0f)
-            .setColor(r, g, b, a)
-            .setUv(u1, v1)
-            .setUv1(0, 0)
-            .setUv2(light and 0xFFFF, light shr 16)
-            .setNormal(0f, 0f, -1f)
-
-        consumer.addVertex(matrix, 1f, 0f, 0f)
-            .setColor(r, g, b, a)
-            .setUv(u1, v0)
-            .setUv1(0, 0)
-            .setUv2(light and 0xFFFF, light shr 16)
-            .setNormal(0f, 0f, -1f)
+        consumer.addVertex(matrix, 0f, 0f, 0f).setColor(r, g, b, a).setUv(0f, 0f)
+            .setUv1(0, 0).setUv2(light and 0xFFFF, light shr 16).setNormal(0f, 0f, -1f)
+        consumer.addVertex(matrix, 0f, 1f, 0f).setColor(r, g, b, a).setUv(0f, 1f)
+            .setUv1(0, 0).setUv2(light and 0xFFFF, light shr 16).setNormal(0f, 0f, -1f)
+        consumer.addVertex(matrix, 1f, 1f, 0f).setColor(r, g, b, a).setUv(1f, 1f)
+            .setUv1(0, 0).setUv2(light and 0xFFFF, light shr 16).setNormal(0f, 0f, -1f)
+        consumer.addVertex(matrix, 1f, 0f, 0f).setColor(r, g, b, a).setUv(1f, 0f)
+            .setUv1(0, 0).setUv2(light and 0xFFFF, light shr 16).setNormal(0f, 0f, -1f)
     }
 }
