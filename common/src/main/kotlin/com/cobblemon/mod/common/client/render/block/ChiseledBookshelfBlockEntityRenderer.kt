ec35c9b9bb72da7ad14bcd5a2515996fd8dda4bb
plastered_crab <jeffw773@gmail.com>
2025-04-22 07:55:36 -0700

    shelf face rendered (upside down...)

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/block/ChiseledBookshelfBlockEntityRenderer.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/block/ChiseledBookshelfBlockEntityRenderer.kt
@@ -21,6 +21,7 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
     BlockEntityRenderer<ChiseledBookshelfBlockEntity> {
 
     private val texture = cobblemonResource("textures/block/chiseled_bookshelf/technical_machine_case.png")
+    private val vanillaFaceTexture = ResourceLocation.parse("minecraft:textures/block/chiseled_bookshelf_empty.png")
 
     // Layout parameters in pixels (relative to 16x16 block face)
     private val slotWidthPixels = 2
@@ -41,6 +42,7 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
         light: Int,
         overlay: Int
     ) {
+
         val facing = entity.blockState.getValue(HorizontalDirectionalBlock.FACING)
         val rotation = when (facing) {
             Direction.NORTH -> 0f
@@ -54,6 +56,9 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
         poseStack.translate(0.5, 0.5, 0.5)
         poseStack.mulPose(Axis.YP.rotationDegrees(rotation))
         poseStack.translate(-0.5, -0.5, -0.5)
+
+        // we render the dumb missing face
+        renderFrontFaceQuad(poseStack, buffer, vanillaFaceTexture, light)
 
         val slotWidth = slotWidthPixels / textureSize
         val slotHeight = slotHeightPixels / textureSize
@@ -124,6 +129,55 @@ class ChiseledBookshelfBlockEntityRenderer(ctx: BlockEntityRendererProvider.Cont
         consumer.addVertex(matrix, width, 0f, 0f)
             .setColor(r, g, b, a)
             .setUv(1f, 0f)
+            .setUv1(0, 0)
+            .setUv2(light and 0xFFFF, light shr 16)
+            .setNormal(0f, 0f, -1f)
+    }
+
+    private fun renderFrontFaceQuad(
+        poseStack: PoseStack,
+        buffer: MultiBufferSource,
+        texture: ResourceLocation,
+        light: Int
+    ) {
+        val consumer = buffer.getBuffer(RenderType.entitySolid(texture))
+        val matrix = poseStack.last().pose()
+
+        val u0 = 0f
+        val v0 = 0f
+        val u1 = 1f
+        val v1 = 1f
+
+        val r = 1.0f
+        val g = 1.0f
+        val b = 1.0f
+        val a = 1.0f
+
+        // Front face is on Z = 0
+        consumer.addVertex(matrix, 0f, 0f, 0f)
+            .setColor(r, g, b, a)
+            .setUv(u0, v0)
+            .setUv1(0, 0)
+            .setUv2(light and 0xFFFF, light shr 16)
+            .setNormal(0f, 0f, -1f)
+
+        consumer.addVertex(matrix, 0f, 1f, 0f)
+            .setColor(r, g, b, a)
+            .setUv(u0, v1)
+            .setUv1(0, 0)
+            .setUv2(light and 0xFFFF, light shr 16)
+            .setNormal(0f, 0f, -1f)
+
+        consumer.addVertex(matrix, 1f, 1f, 0f)
+            .setColor(r, g, b, a)
+            .setUv(u1, v1)
+            .setUv1(0, 0)
+            .setUv2(light and 0xFFFF, light shr 16)
+            .setNormal(0f, 0f, -1f)
+
+        consumer.addVertex(matrix, 1f, 0f, 0f)
+            .setColor(r, g, b, a)
+            .setUv(u1, v0)
             .setUv1(0, 0)
             .setUv2(light and 0xFFFF, light shr 16)
             .setNormal(0f, 0f, -1f)
