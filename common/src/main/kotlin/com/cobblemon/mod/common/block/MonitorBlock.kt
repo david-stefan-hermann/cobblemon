38cd081ad61fe0a56d94433eaaf78e0aa0dd2f52
Plastered_Crab <49732785+PlasteredCrab@users.noreply.github.com>
2026-02-04 13:50:57 -0800

    Data Monitor to eject disk if player interacts with empty hand or non-disk item

--- common/src/main/kotlin/com/cobblemon/mod/common/block/MonitorBlock.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/block/MonitorBlock.kt
@@ -84,7 +84,15 @@ class MonitorBlock(settings: Properties) : MultiblockBlock(settings) {
     ): ItemInteractionResult {
         val entity = level.getBlockEntity(pos) as? FossilMultiblockEntity
             ?: return ItemInteractionResult.SKIP_DEFAULT_BLOCK_INTERACTION
-        return entity.handleUseItem(stack, state, level, pos, player)
+        val result = entity.handleUseItem(stack, state, level, pos, player)
+        if (result == ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION) {
+            return if (entity.handleUseWithoutItem(state, level, pos, player).consumesAction()) {
+                ItemInteractionResult.sidedSuccess(level.isClientSide)
+            } else {
+                ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION
+            }
+        }
+        return result
     }
 
     override fun onRemove(state: BlockState, level: Level, pos: BlockPos, newState: BlockState, movedByPiston: Boolean) {
