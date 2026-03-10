a7998bcec5f7cf70f7577ba9d1d76cf069923e32
Plastered_Crab <49732785+PlasteredCrab@users.noreply.github.com>
2026-03-10 09:01:59 -0700

    Cleanup Obtainment methods for TMs

--- common/src/main/kotlin/com/cobblemon/mod/common/tms/obtain/DefaultObtainMethod.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/tms/obtain/DefaultObtainMethod.kt
@@ -14,27 +14,27 @@ import net.minecraft.network.RegistryFriendlyByteBuf
 import net.minecraft.server.level.ServerPlayer
 
 /**
- * An [ObtainMethod] that unlocks immediately.
+ * An [ObtainMethod] that unlocks immediately by default.
  * Used for TMs that aren't unlocked and are available immediately.
  * TMs with this [ObtainMethod] will not display an unlock text.
  *
- * @author whatsy
+ * @author Plastered_Crab
  */
-class NoneObtainMethod : ObtainMethod {
+class DefaultObtainMethod : ObtainMethod {
     override val passive = true
 
     companion object {
-        val ID = cobblemonResource("none")
+        val ID = cobblemonResource("default")
 
-        fun readFromBuffer(buffer: RegistryFriendlyByteBuf): NoneObtainMethod {
-            return NoneObtainMethod()
+        fun readFromBuffer(buffer: RegistryFriendlyByteBuf): DefaultObtainMethod {
+            return DefaultObtainMethod()
         }
     }
 
     override fun matches(player: ServerPlayer) = true
 
     override fun writeToBuffer(buffer: RegistryFriendlyByteBuf) {
-        buffer.writeUtf("cobblemon:none")
+        buffer.writeUtf("cobblemon:default")
     }
 }
 
