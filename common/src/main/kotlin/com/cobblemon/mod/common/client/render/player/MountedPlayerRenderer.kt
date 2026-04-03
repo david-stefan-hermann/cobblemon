9439070325149aaca1655e4c0c11e42d709af8ba
jackowes <78700961+jackowes@users.noreply.github.com>
2026-04-03 14:07:18 -0500

    fix to mounted player renderer axis. still issues

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/player/MountedPlayerRenderer.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/player/MountedPlayerRenderer.kt
@@ -143,11 +143,11 @@ object MountedPlayerRenderer {
     const val RIDING_SITTING_OFFSET = -10.0
     fun animateRoot(stack: PoseStack) {
         val root = relevantPartsByName["body"] ?: return
-        val rootOffset = Vec3(root.x.toDouble(), root.y.toDouble() + RIDING_SITTING_OFFSET, root.z.toDouble()).scale(1/16.0)
+        val rootOffset = Vec3(-root.x.toDouble(), root.y.toDouble() + RIDING_SITTING_OFFSET, root.z.toDouble()).scale(1/16.0)
         val rotation = Vec3(root.xRot.toDouble(), root.yRot.toDouble(), root.zRot.toDouble())
+        stack.translate(rootOffset.x.toFloat(), rootOffset.y.toFloat(), rootOffset.z.toFloat())
         stack.mulPose(Axis.ZP.rotation(rotation.z.toFloat()))
         stack.mulPose(Axis.YP.rotation(rotation.y.toFloat()))
         stack.mulPose(Axis.XP.rotation(rotation.x.toFloat()))
-        stack.translate(rootOffset.x.toFloat(), rootOffset.y.toFloat(), rootOffset.z.toFloat())
     }
 }
