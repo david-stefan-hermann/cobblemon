499778dd21c25cb2487e44fb46c92ec1b939c9c3
Faintful <174635244+Faintful@users.noreply.github.com>
2026-04-25 01:30:04 +0200

    Refactor dragon type gem cluster

--- common/src/main/kotlin/com/cobblemon/mod/common/CobblemonBlocks.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/CobblemonBlocks.kt
@@ -355,7 +355,7 @@ object CobblemonBlocks : PlatformRegistry<Registry<Block>, ResourceKey<Registry<
     @JvmField
     val TYPE_GEM_CLUSTER_GHOST = typeGemCluster("type_gem_cluster_ghost", TYPE_GEM_BLOCK_GHOST, cobblemonResource("ghost_gem"))
     @JvmField
-    val TYPE_GEM_CLUSTER_DRAGON = typeGemCluster("type_gem_cluster_dragon", TYPE_GEM_BLOCK_DRAGON, cobblemonResource("dragon_gem"))
+    val TYPE_GEM_CLUSTER_DRAGON = typeGemCluster("dragon_gem_cluster", TYPE_GEM_BLOCK_DRAGON, cobblemonResource("dragon_gem"))
     @JvmField
     val TYPE_GEM_CLUSTER_DARK = typeGemCluster("type_gem_cluster_dark", TYPE_GEM_BLOCK_DARK, cobblemonResource("dark_gem"))
     @JvmField
--- common/src/main/kotlin/com/cobblemon/mod/common/CobblemonItems.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/CobblemonItems.kt
@@ -1246,7 +1246,7 @@ object CobblemonItems : PlatformRegistry<Registry<Item>, ResourceKey<Registry<It
     @JvmField
     val TYPE_GEM_CLUSTER_GHOST = blockItem("type_gem_cluster_ghost", CobblemonBlocks.TYPE_GEM_CLUSTER_GHOST)
     @JvmField
-    val TYPE_GEM_CLUSTER_DRAGON = blockItem("type_gem_cluster_dragon", CobblemonBlocks.TYPE_GEM_CLUSTER_DRAGON)
+    val TYPE_GEM_CLUSTER_DRAGON = blockItem("dragon_gem_cluster", CobblemonBlocks.TYPE_GEM_CLUSTER_DRAGON)
     @JvmField
     val TYPE_GEM_CLUSTER_DARK = blockItem("type_gem_cluster_dark", CobblemonBlocks.TYPE_GEM_CLUSTER_DARK)
     @JvmField
