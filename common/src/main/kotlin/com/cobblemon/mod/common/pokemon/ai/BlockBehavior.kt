a2b62bea48848604255562d0d9ec4340e40cf9db
JazzMcNade <pharoalexander@gmail.com>
2026-04-24 19:12:06 -0500

    Fixed molang query function

--- common/src/main/kotlin/com/cobblemon/mod/common/pokemon/ai/BlockBehavior.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/pokemon/ai/BlockBehavior.kt
@@ -19,7 +19,7 @@ class BlockBehavior {
     @Transient
     val struct = ObjectValue(this).also {
         it.addFunction("immune_to_sweet_berry_bush_block") { DoubleValue(immuneToSweetBerryBushBlock) }
-        it.addFunction("immune_to_cobweb_block") { DoubleValue(immuneToSweetBerryBushBlock) }
+        it.addFunction("immune_to_cobweb_block") { DoubleValue(immuneToCobwebBlock) }
         it.addFunction("can_stand_on_powder_snow") { DoubleValue(canStandOnPowderSnow) }
         it.addFunction("can_path_through_sacc_leaves") { DoubleValue(canPathThroughSaccLeaves) }
     }
